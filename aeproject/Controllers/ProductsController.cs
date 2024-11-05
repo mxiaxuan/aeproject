2a5b90c72473269f61b200701cf96b103b3c43ee
st4u2636025252@gmail.com <st4u2636025252@gmail.com>
2024-11-05 16:36:38 +0800

    新增產品頁面

---
+++ aeproject/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using aeproject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aeproject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AespadbContext _context;
+
+        public ProductsController(AespadbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View("Products", products); // 指定使用 Products.cshtml 檔案
+        }
+    }
+}
+
+
